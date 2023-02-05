a4c90df0173aa5e5c107ac524b9492c81f25c4e5
Wiktoria Więcek <wiecekw18@gmail.com>
2023-02-05 16:16:01 +0100

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+from nltk.metrics.distance import edit_distance
+import advertools as adv
+from nltk import word_tokenize
+import nltk
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+import Pycluster as PC
+
+# settings
+nltk.download('punkt')
+
+def cleaning_data(keywords, lan = 'polish'):
+    stopwords = (list(adv.stopwords[lan]))
+    words = word_tokenize(keywords)
+    words = [word for word in words if word not in stopwords]
+    words = TreebankWordDetokenizer().detokenize(words)
+    return words
+
+header = st.container()
+dataset = st.container()
+model = st.container()
+results = st.container()
+
+with header:
+    st.title("Clustering keywords")
+
+with dataset:
+    # input a keywords
+    st.header("Please write your keywords")
+    kw_input = pd.DataFrame(st.text_area("IMPORTANT! Each keyword should be in new line").split('\n'))
+    kw_input = kw_input[0].tolist()
+    st.write(kw_input)
+    lan = st.selectbox("Choose the language", options=["polish", "english", "spanish"], index=0)
+    nr_clusters = st.number_input(label='How many clusters?', min_value=2, key=2)
+
+with model:
+    keywords = cleaning_data(kw_input)
+
+    # Edit distance - vector
+    dist = [edit_distance(keywords[i], keywords[j])
+            for i in range(1, len(keywords))
+            for j in range(0,i)]
+
+    labels, error, nfound = PC.kmedoids(dist, nclusters=nr_clusters)
+
+    st.write("How many errors? {}" .format(len(error)))
+    st.write("How many outliers? {}" .format(len(nfound)))
+
+    st.write("You want to see the groups?")
+    cluster = dict()
+    
+with results:
+    for word, label in zip(keywords, labels):
+        cluster.setdefault(label, []).append(word)
+    for label, grp in cluster.items():
+        str.write(grp)
+
+
+
